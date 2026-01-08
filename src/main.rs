592414f25c7a8f522e8399d8a4d1ad63bd65657b
Kelechi Ebiri <ebiritg@gmail.com>
2026-01-08 23:54:59 +0100

    add main file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use jot::configuration::get_configuration;
+use jot::issue_delivery_worker::run_worker_until_stopped;
+use jot::startup::Application;
+use jot::telemetry::{get_subscriber, init_subscriber};
+use std::fmt::{Debug, Display};
+use tokio::task::JoinError;
+
+async fn main() -> anyhow::Result<()> {
+    let subscriber = get_subcriber("jot".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let application = Application::build(configuration.clone()).await?;
+
+    Ok(())
 }
