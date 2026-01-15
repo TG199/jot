6924164f544c328b504c260d7d69ab30216335b2
Kelechi Ebiri <ebiritg@gmail.com>
2026-01-15 12:52:23 +0100

    prepare notes endpoint

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,5 +1,7 @@
 mod health_check;
 mod home;
+mod notes;
 
 pub use health_check::*;
 pub use home::*;
+pub use notes::*;
--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,7 @@ use crate::configuration::DatabaseSettings;
 use crate::configuration::Settings;
 use crate::routes::health_check;
 use crate::routes::home;
+use crate::routes::post_note;
 
 use actix_session::SessionMiddleware;
 use actix_session::storage::RedisSessionStore;
@@ -26,7 +27,7 @@ pub struct Application {
 
 impl Application {
     pub async fn build(configuration: Settings) -> Result<Self, anyhow::Error> {
-        // let connection_pool = get_connection_pool(&configuration.database);
+        let connection_pool = get_connection_pool(&configuration.database);
 
         let address = format!(
             "{}:{}",
@@ -37,10 +38,9 @@ impl Application {
         let port = listener.local_addr().unwrap().port();
         let server = run(
             listener,
-            // connection_pool,
+            connection_pool,
             configuration.application.url().expect("Invalid host url"),
             configuration.application.hmac_secret,
-            configuration.redis_uri,
         )
         .await?;
 
@@ -66,30 +66,25 @@ pub struct ApplicationBaseUrl(pub Url);
 
 async fn run(
     listener: TcpListener,
-    // db_pool: PgPool,
+    db_pool: PgPool,
     base_url: Url,
     hmac_secret: SecretString,
-    redis_uri: SecretString,
 ) -> Result<Server, anyhow::Error> {
-    // let db_pool = web::Data::new(db_pool);
+    let db_pool = web::Data::new(db_pool);
     let base_url = web::Data::new(ApplicationBaseUrl(base_url));
 
     let secret_key = Key::from(hmac_secret.expose_secret().as_bytes());
     let message_store = CookieMessageStore::builder(secret_key.clone()).build();
     let message_framework = FlashMessagesFramework::builder(message_store).build();
 
-    // let redis_store = RedisSessionStore::new(redis_uri.expose_secret()).await?;
     let server = HttpServer::new(move || {
         App::new()
             .wrap(message_framework.clone())
-            // .wrap(SessionMiddleware::new(
-            //     redis_store.clone(),
-            //     secret_key.clone(),
-            // ))
             .wrap(TracingLogger::default())
-            .route("/health", web::get().to(health_check))
             .route("/", web::get().to(home))
-            // .app_data(db_pool.clone())
+            .route("/health", web::get().to(health_check))
+            .route("/notes", web::post().to(post_note))
+            .app_data(db_pool.clone())
             .app_data(base_url.clone())
             .app_data(web::Data::new(HmacSecret(hmac_secret.clone())))
     })
