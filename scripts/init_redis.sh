aeae644dc4b9747bd5749c723600f00d3e710942
Kelechi Ebiri <ebiritg@gmail.com>
2026-01-23 12:09:00 +0100

    feat: add init_redis script to set up Redis container

---
+++ scripts/init_redis.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -x
+set -eo pipefail
+
+REDIS_PORT="${REDIS_PORT:=6379}"
+
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+  docker run \
+    -p "${REDIS_PORT}":6379 \
+    -d redis:7 \
+    redis-server
+fi
+
+>&2 echo "Redis is ready to go on port ${REDIS_PORT}!"
