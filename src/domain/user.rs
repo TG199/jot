f9d2613a8569af4ca07e7d0caeb4cebe4a8e2594
Kelechi Ebiri <ebiritg@gmail.com>
2026-02-11 19:36:24 +0100

    feat: implement User and NewUser structs with parsing functionality

---
+++ src/domain/user.rs
@@ -0,0 +1,26 @@
+use super::{UserEmail, UserPassword};
+use chrono::{DateTime, Utc};
+use uuid::Uuid;
+
+#[derive(Debug, Clone)]
+pub struct User {
+    pub user_id: Uuid,
+    pub email: UserEmail,
+    pub password_hash: String,
+    pub created_at: DateTime<Utc>,
+    pub updatedt_at: DateTime<Utc>,
+}
+
+#[derive(Debug, Clone)]
+pub struct NewUser {
+    pub email: UserEmail,
+    pub password: UserPassWord,
+}
+
+impl NewUser {
+    pub fn parse(email: String, password: String) -> Result<NewUser, String> {
+        let email = UserEmail::parse(email)?;
+        let password = UserPassWord::parse(password)?;
+        Ok(Self { email, password })
+    }
+}
