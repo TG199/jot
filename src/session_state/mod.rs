4a5c2be83cabbdbec0279b76d50862edd80fa348
Kelechi Ebiri <ebiritg@gmail.com>
2026-02-14 11:06:17 +0100

    feat: add session middleware with Redis session store

---
+++ src/session_state/mod.rs
@@ -0,0 +1,15 @@
+use actix_session::storage::RedisSessionStore;
+use actix_session::SessionMiddleware;
+use actix_web::cookie::Key;
+use secrecy::ExposeSecret;
+use secrecy::SecretString;
+
+pub fn session_middleware(
+    redis_uri: SecretString,
+    secret_key: Key,
+) -> Result<SessionMiddleware<RedisSessionStore>, anyhow::Error> {
+    let redis_store = RedisSessionStore::new(redis_store.expose_secret())
+        .map_err(|e| anyhow::anyhow!("Failed to  connect to Redis: {}", e))?;
+
+    Ok(SessionMiddleware::new(redis_store, secret_key))
+}
