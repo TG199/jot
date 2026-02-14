b17006d4b9e3557561bb445f8858991259cec9b9
Kelechi Ebiri <ebiritg@gmail.com>
2026-02-14 17:30:09 +0100

    add modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
+mod authentication;
 pub mod configuration;
 mod domain;
 pub mod routes;
+mod session_state;
 pub mod startup;
 pub mod telemetry;
 pub mod utils;
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,9 +1,13 @@
 mod health_check;
 mod home;
+mod login;
+mod logout;
 mod notes;
 mod users;
 
 pub use health_check::*;
 pub use home::*;
+pub use login::*;
+pub use logout::*;
 pub use notes::*;
 pub use users::*;
