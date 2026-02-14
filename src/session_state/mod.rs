d61c778d7561e8a00779daefefe051bb49df742b
Kelechi Ebiri <ebiritg@gmail.com>
2026-02-14 17:39:58 +0100

    feat: update session_middleware to be asynchronous and handle Redis connection error

--- src/session_state/mod.rs
+++ src/session_state/mod.rs
@@ -1,15 +1,17 @@
 use actix_session::storage::RedisSessionStore;
 use actix_session::SessionMiddleware;
 use actix_web::cookie::Key;
+use futures::TryFutureExt;
 use secrecy::ExposeSecret;
 use secrecy::SecretString;
 
-pub fn session_middleware(
+pub async fn session_middleware(
     redis_uri: SecretString,
     secret_key: Key,
 ) -> Result<SessionMiddleware<RedisSessionStore>, anyhow::Error> {
     let redis_store = RedisSessionStore::new(redis_uri.expose_secret())
-        .map_err(|e| anyhow::anyhow!("Failed to  connect to Redis: {}", e))?;
+        .map_err(|e| anyhow::anyhow!("Failed to  connect to Redis: {}", e))
+        .await;
 
     Ok(SessionMiddleware::new(redis_store, secret_key))
 }
