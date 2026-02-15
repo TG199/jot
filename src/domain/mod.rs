399c793bbdacd40a94608506c62a10884fb007ab
Kelechi Ebiri <ebiritg@gmail.com>
2026-02-15 01:09:00 +0100

    feat: add unicode-segmentation dependency and create initial route files for notes

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -5,6 +5,9 @@ mod user;
 mod user_email;
 mod user_password;
 
+pub use note::*;
+pub use note_content::*;
+pub use note_title::*;
 pub use user::*;
 pub use user_email::*;
 pub use user_password::*;
