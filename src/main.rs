8f0dd6b852ead0f38bd6b39c54fc1ef481b3bd60
Kelechi Ebiri <ebiritg@gmail.com>
2026-02-23 14:50:18 +0100

    feat: enhance logging setup by determining log level from configuration

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use jot::configuration::get_configuration;
 use jot::startup::Application;
-use jot::telemetry::{get_subscriber, init_subscriber};
+use jot::telemetry::{get_log_level_for_env, get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let subscriber = get_subscriber("jot".into(), "info".into(), std::io::stdout);
+    let configuration = get_configuration().expect("Failed to read configuration");
+
+    let log_level = get_log_level_for_env(&configuration.application.environment);
+    let subscriber = get_subscriber("jot".into(), log_level, std::io::stdout);
     init_subscriber(subscriber);
 
-    let configuration = get_configuration().expect("Failed to read configuration");
+    tracing::info!(
+        environment = %configuration.application.environment,
+        "Starting Jot API on {}:{}",
+        configuration.application.host,
+        configuration.application.port,
+    );
+
     let application = Application::build(configuration.clone()).await?;
     application.run_until_stopped().await?;
 
